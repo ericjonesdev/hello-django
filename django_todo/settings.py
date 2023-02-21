723dc49f4b82c4b76e81ea18710361e1361cf88d
ericjonesdev <ericjones2004@yahoo.com>
2023-02-21 19:20:48 +0000

    Setup environment variables and automatic deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ericjonesde-hellodjango-6xy3h3tfgjs.ws-eu87.gitpod.io', 'hellodjango.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
