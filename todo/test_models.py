9e3694cc97c2584f27e2b3888552d82951ea1d7a
ericjonesdev <ericjones2004@yahoo.com>
2023-02-21 13:15:32 +0000

    Complete tests and add readme file

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
+from .models import Item
 
-# Create your tests here.
 
-class TestDjango(TestCase):
+class TestModels(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
+
+
+    def test_item_string_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
