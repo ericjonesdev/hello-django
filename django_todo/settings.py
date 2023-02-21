37c54be67e12092b7e9c46064d411068965cd8f3
ericjonesdev <ericjones2004@yahoo.com>
2023-02-21 16:38:36 +0000

    Remove import env standalone statement from line 19/settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,7 +16,6 @@ import os
 if os.path.isfile("env.py"):
     import env
 import dj_database_url
-import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
