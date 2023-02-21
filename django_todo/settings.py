d0b6f943352ad3bcce1a999d3be2e64fe6de57fb
ericjonesdev <ericjones2004@yahoo.com>
2023-02-21 19:08:18 +0000

    Add allowed host URL

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ericjonesde-hellodjango-6xy3h3tfgjs.ws-eu87.gitpod.io']
 
 
 # Application definition
